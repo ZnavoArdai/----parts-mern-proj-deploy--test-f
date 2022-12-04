95254ef2d120282b4d8207aaf45d4830f259f300
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-12-04 21:38:32 +0200

    x

--- client/src/services/ordersService.js
+++ client/src/services/ordersService.js
@@ -2,7 +2,7 @@
 
 export const getOrders =async () => {
 
-    const respo=await fetch("http://localhost:8080/orders");
+    const respo=await fetch("https://znavo-test-b.onrender.com/orders");
 
     const orders=await respo.json();
     return orders;
