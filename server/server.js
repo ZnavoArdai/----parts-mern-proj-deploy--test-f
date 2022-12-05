7a35d518f6cf37d3f0c40b8e714e9c7885116b68
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-12-05 02:55:00 +0200

    x

--- server/server.js
+++ server/server.js
@@ -47,6 +47,5 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res)=>{
       res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-}
+  });}
 
